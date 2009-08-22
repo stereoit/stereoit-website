2c28ee2841b8c09af0ab31e94294e15a83aaa441
Robert Smol <robert.smol@stereoit.com>
2009-08-22 17:33:16 +0200

    Bisected index.html into templates.
    Restructured static index.html into many smaller template tags.

--- website/settings.py
+++ website/settings.py
@@ -71,7 +71,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	os.path.join(os.path.basename(__file__), 'templates'),
+	'%s/templates/' % os.path.dirname(os.path.abspath(__file__)),
 )
 
 INSTALLED_APPS = (

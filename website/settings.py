601273cc5b1d059957dec27954081a5119b2e41b
Robert Smol <robert.smol@stereoit.com>
2009-08-22 16:39:35 +0200

    Modified settings.py do dynamically find paths.
    We can now automatically find template and static media paths.

--- website/settings.py
+++ website/settings.py
@@ -37,8 +37,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-#MEDIA_ROOT = os.path.join(os.path.basename(__file__), 'static')
-MEDIA_ROOT = '/home/rsmol/Code/jquery/website/static/'
+MEDIA_ROOT = '%s/static/' % os.path.dirname(os.path.abspath(__file__))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -72,7 +71,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'/home/rsmol/Code/jquery/website/templates',
 	os.path.join(os.path.basename(__file__), 'templates'),
 )
 
